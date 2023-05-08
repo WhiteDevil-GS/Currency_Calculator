bfe90566f462fd25047e3f2f4eee2b0d61bdecab
Manoj28_MJ <123579963+ManojManu9886@users.noreply.github.com>
2023-05-08 16:49:12 +0000

    added a format string to get commas

--- currency_cal_project.py
+++ currency_cal_project.py
@@ -39,6 +39,7 @@ while True:
                     a1 = 80.99
                     usd = float(b) / float(a1)
                     sum1 = sum1 + float(usd)
+                    sum1 = (f"{sum1:,}")
                 else:
                     print(f"The ammount in US Dollars is = ${sum1}")
                     break
@@ -52,6 +53,7 @@ while True:
                     a2 = 78.5
                     ero = float(b) / float(a2)
                     sum2 = sum2 + float(ero)
+                    sum2 = (f"{sum2:,}")
                 else:
                     print(f"The ammount in Euro is = €{sum2}")
                     break
@@ -65,6 +67,7 @@ while True:
                     a3 = 87.91
                     pound = float(b) / float(a3)
                     sum3 = sum3 + float(pound)
+                    sum3 = (f"{sum3:,}")
                 else:
                     print(f"The amount in Pound is = £{sum3}")
                     break
@@ -78,6 +81,7 @@ while True:
                     a4 = 52.89
                     ad = float(b) / float(a4)
                     sum4 = sum4 + float(ad)
+                    sum4 = (f"{sum4:,}")
                 else:
                     print(f"The ammount in Australian Dollars is = A${sum4}")
                     break
@@ -91,6 +95,7 @@ while True:
                     a5 = 22.05
                     uae = float(b) / float(a5)
                     sum5 = sum5 + float(uae)
+                    sum5 = (f"{sum5:,}")
                 else:
                     print(f"The ammount in UAE Dirham is = د.إ{sum5}")
                     break
@@ -104,6 +109,7 @@ while True:
                     a6 = 46.53
                     nz = float(b) / float(a6)
                     sum6 = sum6 + float(nz)
+                    sum6 = (f"{sum6:,}")
                 else:
                     print(f"The ammount in New Zealand Dollar is = ${sum6}")
                     break
@@ -117,6 +123,7 @@ while True:
                     a7 = 59.6
                     cand = float(b) / float(a7)
                     sum7 = sum7 + float(cand)
+                    sum7 = (f"{sum7:,}")
                 else:
                     print(f"The ammount in canadian dollar is = ${sum7}")
                     break
@@ -130,6 +137,7 @@ while True:
                     a8 = 82.55
                     swf = float(b) / float(a8)
                     sum8 = sum8 + float(swf)
+                    sum8 = (f"{sum8:,}")
                 else:
                     print(f"The ammount in Swiss Franc is = Fr{sum8}")
                     break
@@ -143,6 +151,7 @@ while True:
                     a9 = 0.565
                     jyen = float(b) / float(a9)
                     sum9 = sum9 + float(jyen)
+                    sum9 = (f"{sum9:,}")
                 else:
                     print(f"The ammount in japanese yen is = ¥{sum9}")
                     break
@@ -156,6 +165,7 @@ while True:
                     a10 = 21.60
                     saudi = float(b) / float(a10)
                     sum10 = sum10 + float(saudi)
+                    sum10 = (f"{sum10:,}")
                 else:
                     print(f"The ammount in Saudi Riyal is = SAR{sum10}")
                     break
@@ -169,6 +179,7 @@ while True:
                     a11 = 22.25
                     qr = float(b) / float(a11)
                     sum11 = sum11 + float(qr)
+                    sum11 = (f"{sum11:,}")
                 else:
                     print(f"The ammount in Qatari Rial is = ر.ق{sum11}")
                     break
@@ -182,6 +193,7 @@ while True:
                     a12 = 210.53
                     orial = float(b) / float(a12)
                     sum12 = sum12 + float(orial)
+                    sum12= (f"{sum12:,}")
                 else:
                     print(f"The ammount in Omani Rial is = ر.ع.{sum12}")
                     break
@@ -195,6 +207,7 @@ while True:
                     a13 = 216.1
                     bha = float(b) / float(a13)
                     sum13 = sum13 + float(bha)
+                    sum13 = (f"{sum13:,}")
                 else:
                     print(f"The ammount in Bahraini Dinar is = دينا{sum13}")
                     break
@@ -208,6 +221,7 @@ while True:
                     a14 = 261.79
                     kuwa = float(b) / float(a14)
                     sum14 = sum14 + float(kuwa)
+                    sum14 = (f"{sum14:,}")
                 else:
                     print(f"The ammount in Kuwaiti Dinar is كويتي{sum14}")
                     break
@@ -221,6 +235,7 @@ while True:
                     a15 = 56.63
                     sing = float(b) / float(a15)
                     sum15 = sum15 + float(sing)
+                    sum15 = (f"{sum15:,}")
                 else:
                     print(f"The ammount in Singapore Dollar is = S${sum15}")
                     break
@@ -234,6 +249,7 @@ while True:
                     a16 = 17.7
                     mal = float(b) / float(a16)
                     sum16 = sum16 + float(mal)
+                    sum16 = (f"{sum16:,}")
                 else:
                     print(f"The ammount in malaysian ringgit is = RM{sum16}")
                     break
@@ -247,6 +263,7 @@ while True:
                     a17 = 7.17
                     ssed = float(b) / float(a17)
                     sum17 = sum17 + float(ssed)
+                    sum17 = (f"{sum17:,}")
                 else:
                     print(f"The ammount in Swedish Krona is = kr{sum17}")
                     break
@@ -260,6 +277,7 @@ while True:
                     a18 = 10.56
                     dk = float(b) / float(a18)
                     sum18 = sum18 + float(dk)
+                    sum18 = (f"{sum18:,}")
                 else:
                     print(f"The ammount in danish krone is = Kr.{sum18}")
                     break
@@ -273,6 +291,7 @@ while True:
                     a19 = 2.16
                     tb = float(b) / float(a19)
                     sum19 = sum19 + float(tb)
+                    sum19 = (f"{sum19:,}")
                 else:
                     print(f"The ammount in Thai Baht is = ฿{sum19}")
                     break
@@ -286,6 +305,7 @@ while True:
                     a20 = 10.32
                     hkd = float(b) / float(a20)
                     sum20 = sum20 + float(hkd)
+                    sum20 = (f"{sum20:,}")
                 else:
                     print(f"The ammount in Hong Kong Dollar is = HK${sum20}")
                     break
@@ -299,6 +319,7 @@ while True:
                     a21 = 4.52
                     sarf = float(b) / float(a21)
                     sum21 = sum21 + float(sarf)
+                    sum21 = (f"{sum21:,}")
                 else:
                     print(f"The ammount in South African Rand is = R{sum21}")
                     break
@@ -312,6 +333,7 @@ while True:
                     a22 = 11.36
                     chi = float(b) / float(a22)
                     sum22 = sum22 + float(chi)
+                    sum22 = (f"{sum22:,}")
                 else:
                     print(f"The ammount in Chinese Yuan is = ¥{sum22}")
                     break
